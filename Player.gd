04b42948e472f341e704ae4ef7d491d4ab9d0950
Marcus Sanatan <msanatan@gmail.com>
2023-09-25 20:59:11 -0400

    Adjust player script so it works in v4
    The jump is too floaty, but we can adjust gravity for that I guess

--- Player.gd
+++ Player.gd
@@ -1,32 +1,33 @@
-extends KinematicBody
+extends CharacterBody3D
 
-export var speed = 15
-export var fall_acceleration = 75
-export var jump_impulse = 20
-var velocity = Vector3.ZERO
+@export var speed = 5
+@export var jump_impulse = 3
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 func _physics_process(delta):
-    var direction = Vector3.ZERO
+	var direction = Vector3.ZERO
+	
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	
+	if is_on_floor() and Input.is_action_pressed("jump"):
+		velocity.y += jump_impulse
 
-    if Input.is_action_pressed("move_right"):
-        direction.x += 1
-    elif Input.is_action_pressed("move_left"):
-        direction.x -= 1
-    
-    if Input.is_action_pressed("move_back"):
-        direction.z += 1
-    elif Input.is_action_pressed("move_forward"):
-        direction.z -= 1
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	elif Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	elif Input.is_action_pressed("move_forward"):
+		direction.z -= 1
 
-    if direction != Vector3.ZERO:
-        direction = direction.normalized()
-    
-    velocity.x = direction.x * speed
-    velocity.z = direction.z * speed
-    velocity.y -= fall_acceleration * delta 
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+	
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
 
-    if is_on_floor() and Input.is_action_pressed("jump"):
-        velocity.y += jump_impulse
-
-    velocity = move_and_slide(velocity, Vector3.UP)
+	move_and_slide()
